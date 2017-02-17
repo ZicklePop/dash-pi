fea58403f6344058deb9b2b47011ddf7901b6c77
Jake Bilbrey <me@jakebilbrey.com>
2017-02-16 18:46:23 -0800

    Consolidating routes

--- server.js
+++ server.js
@@ -4,8 +4,12 @@
 const next = require('next')
 const Hapi = require('hapi')
 const Good = require('good')
-const routes = require('./routes')
+const darkSky = require('./methods/darkSky')
+const myHotPics = require('./methods/myHotPics')
+const nextWrapper = require('./utils/nextWrapper')
 
+const pathWrapper = nextWrapper.pathWrapper
+const defaultHandlerWrapper = nextWrapper.defaultHandlerWrapper
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const server = new Hapi.Server()
@@ -16,7 +20,39 @@ app.prepare().then(() => {
     host: '0.0.0.0'
   })
 
-  server.route(routes)
+  server.route([
+    {
+      method: 'GET',
+      path: '/{p*}',
+      config: {
+        handler: defaultHandlerWrapper(app)
+      }
+    },
+    {
+      method: 'GET',
+      path: '/api/weather',
+      config: {
+        pre: [
+          { method: darkSky, assign: 'weather' }
+        ],
+        handler: (req, reply) => {
+          return reply(req.pre.weather)
+        }
+      }
+    },
+    {
+      method: 'GET',
+      path: '/api/gif',
+      config: {
+        pre: [
+          { method: myHotPics, assign: 'gif' }
+        ],
+        handler: (req, reply) => {
+          return reply(req.pre.gif)
+        }
+      }
+    }
+  ])
 
   server.register({
     register: Good,
